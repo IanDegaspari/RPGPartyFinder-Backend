e37eea11a0f5eae39855a62642714c90fb45a659
andresrib <andresrib3@gmail.com>
2021-10-26 19:42:39 -0300

    feat new login route

--- FastAPI/routes/login.py
+++ FastAPI/routes/login.py
@@ -8,6 +8,7 @@ from fastapi import (
 )
 from datetime import timedelta
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer
 
 import os
 import sys
@@ -24,7 +25,59 @@ import cv2
 
 login_router = APIRouter()
 
+
 @login_router.post("/token")
+async def login_for_access_token(
+    response: Response, inputdata: OAuth2PasswordBearer = Depends(), db: Session = Depends(get_db)
+):
+    """realiza o login do usuario"""
+    data = ""
+    # autentica
+    user = await login.authenticate_user(inputdata.username, inputdata.password, db)
+    # retorna errro
+    if not user:
+        response.status_code = fastapi_status.HTTP_401_UNAUTHORIZED
+        return {
+            "access_token": "",
+            "token_type": "",
+            "id": "",
+            "login": "Invalid input",
+            "name": "",
+            "email": ""
+        }
+    else:
+        response.status_code = fastapi_status.HTTP_202_ACCEPTED
+        # define o tempo de expirar o token
+        access_token_expires = timedelta(minutes=login.ACCESS_TOKEN_EXPIRE_MINUTES)
+        # criar um token baseado no tempo e no usuario
+        # pega o role do banco
+        access_token = login.create_access_token(
+            data={"sub": user}, expires_delta=access_token_expires
+        )
+        data = await login.retrieve_login_information(db, user)
+        # retorna o TOKEN gerado e um header falando q eh um bearer
+        if data:
+            token_data = {
+                "access_token": access_token,
+                "token_type": "bearer",
+                "id": data.id,
+                "login": user,
+                "name": data.name,
+                "email": data.email,
+            }
+        else:
+            token_data = {
+                "access_token": access_token,
+                "token_type": "bearer",
+                "id": "",
+                "login": "",
+                "name": "",
+                "email": ""
+            }
+    return token_data
+
+
+@login_router.post("/token/json")
 async def login_for_access_token(
     response: Response, inputdata: Login, db: Session = Depends(get_db)
 ):
